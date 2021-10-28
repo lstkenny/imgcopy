ad0836bf4bfaadd88079b35937389986cb7f28e9
Kenny <kenny@origits.com>
2021-10-28 16:07:59 +0700

    Add image size settings (data-width, data-height); Add tiling size settings (data-rows, data-cols); Optimize multiple images processing;

--- js/index.js
+++ js/index.js
@@ -21,7 +21,6 @@ function addStylesheetRules (rules) {
 		)
 	}
 }
-
 function getTileSrc(image, x, y, w, h) {
 	const cnv = document.createElement("canvas")
 	cnv.width = w
@@ -36,7 +35,6 @@ function getTileSrc(image, x, y, w, h) {
 	)
 	return cnv.toDataURL()
 }
-
 function shuffle(a) {
 	let ci = a.length, 
 		ri
@@ -47,7 +45,6 @@ function shuffle(a) {
 	}
 	return a
 }
-
 function* getId(range = 100) {
 	let index = Date.now() % 1000000
 	while (true) {
@@ -55,70 +52,88 @@ function* getId(range = 100) {
 		yield `t${index.toString(16)}`
 	}
 }
-
-function cutImage(image, pcs = 10) {
-	const pw = image.width / pcs
-	const ph = image.height / pcs
+function createTiles(image, cols, rows) {
+	const width = image.width / cols
+	const height = image.height / rows
 	const tiles = []
-	const rs = (100 / (pcs - 1))
-	for (let x = 0; x < pcs; ++x) {
-		for (let y = 0; y < pcs; ++y) {
-			const rx = x * rs
-			const ry = y * rs
+	const relWidth = (100 / (cols - 1))
+	const relHeight = (100 / (rows - 1))
+	for (let col = 0; col < cols; col++) {
+		for (let row = 0; row < rows; row++) {
+			const relX = col * relWidth
+			const relY = row * relHeight
 			tiles.push({
-				x, y, 
-				rx, ry, 
-				pw, ph,
-				src: getTileSrc(image, x, y, pw, ph)
+				col, row,
+				relX, relY,
+				width, height,
+				src: getTileSrc(image, col, row, width, height)
 			})
 		}
 	}
-	// return shuffle(tiles)
-	return tiles
+	return shuffle(tiles)
 }
-
-const imageSelector = '[data-type="pimg"]'
-const imageContainerClass = "pimg-container"
-const pcs = 10
-const idGen = getId()
-
-const styles = [
-	[`.${imageContainerClass}`,
-		["position", "relative"]
-	],
-	[`.${imageContainerClass} div`,
-		["position", "absolute"],
-		["top", "0"],
-		["right", "0"],
-		["bottom", "0"],
-		["left", "0"],
-		["background-repeat", "no-repeat"]
-	]
-]
-
-document.querySelectorAll(imageSelector).forEach(container => {
+function createPuzzle(img, container) {
+	const cols = container.dataset.cols || 10
+	const rows = container.dataset.rows || 10
+	const colSize = 100 / cols
+	const rowSize = 100 / rows
+	const tiles = createTiles(img, cols, rows)
+	const styles = []
+	tiles.forEach(tile => {
+		const layer = document.createElement("div")
+		layer.id = idGen.next().value
+		styles.push([
+			`#${layer.id}`,
+				["background-image", `url(${tile.src})`],
+				["background-size", `${colSize}% ${rowSize}%`],
+				["background-position", `${tile.relX}% ${tile.relY}%`],
+		])
+		container.appendChild(layer)
+	})
+	return styles
+}
+function makeProtectedPicture(container) {
 	const img = document.createElement("img")
 	img.setAttribute("src", `${container.dataset.base}/${container.dataset.src}`)
 	img.setAttribute("crossOrigin", "anonymous")
+	container.style.width = `${container.dataset.width || img.width}px`
+	container.style.height = `${container.dataset.height || img.height}px`
 	container.removeAttribute("data-base")
 	container.removeAttribute("data-src")
-	img.addEventListener("load", e => {
-		const pieces = cutImage(img, pcs)
-		container.setAttribute("class", imageContainerClass)
-		container.style.width = `${container.dataset.width || img.width}px`
-		container.style.height = `${container.dataset.height || img.height}px`
-		let id = Date.now()
-		pieces.forEach(tile => {
-			const layer = document.createElement("div")
-			layer.id = idGen.next().value
-			styles.push([
-				`#${layer.id}`,
-					["background-image", `url(${tile.src})`],
-					["background-size", `${100 / pcs}%`],
-					["background-position", `${tile.rx}% ${tile.ry}%`],
-			])
-			container.appendChild(layer)
+	// for (let name in container.dataset) {
+	// 	container.removeAttribute(`data-${name}`)
+	// }
+	return new Promise((resolve, reject) => {
+		img.addEventListener("load", e => {
+			resolve(createPuzzle(img, container))
 		})
-		addStylesheetRules(styles)
 	})
-})
+}
+async function processImages(imageSelector) {
+	const imageContainerClass = "pimg-container"
+	let styles = [
+		[`.${imageContainerClass}`,
+			["position", "relative"]
+		],
+		[`.${imageContainerClass} div`,
+			["position", "absolute"],
+			["top", "0"],
+			["right", "0"],
+			["bottom", "0"],
+			["left", "0"],
+			["background-repeat", "no-repeat"]
+		]
+	]
+	let rules = []
+	document.querySelectorAll(imageSelector).forEach(container => {
+		container.setAttribute("class", imageContainerClass)
+		rules.push(makeProtectedPicture(container))
+	})
+	rules = await Promise.all(rules)
+	rules.forEach(rule => {
+		styles = styles.concat(rule)
+	})
+	addStylesheetRules(styles)
+}
+const idGen = getId()
+processImages('[data-type="pimg"]')
